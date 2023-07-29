0cafe3ac805299ec177709fd003fd6e33ea6ea3d
ayman3340 <ayman3340.com>
2023-07-29 13:43:27 +0300

    finish

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,22 +5,22 @@ import book3 from "../assets/book3.png";
 
 export default function Book() {
   return (
-    <div>
-      <h1 className='text-center font-bold m-5'>افضل الكتب للمساعدة في علاج ادمان المخدرات</h1>
-      <div className='row  m-5 justify-content-center align-items-center'>
+    <div className=''>
+      <h1 className='text-center font-bold m-5 '>افضل الكتب للمساعدة في علاج ادمان المخدرات</h1>
+      <div className='row m-5 justify-content-center align-items-center'>
 
   
-<div className="card m-5" style={{width: '18rem' , background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
+<div className="card m-3" style={{width: '12rem' , background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
   <img className="card-img-top" src={book1} alt="profile"  />
   <div className="card-body"style={{background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
   </div>
 </div>
-<div className="card m-5" style={{width: '18rem' , background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
+<div className="card m-3" style={{width: '12rem' , background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
   <img className="card-img-top" src={book2} alt="profile"  />
   <div className="card-body"style={{background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
   </div>
 </div>
-<div className="card m-5" style={{width: '18rem' , background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
+<div className="card m-3" style={{width: '12rem' , background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
   <img className="card-img-top" src={book3} alt="profile"  />
   <div className="card-body"style={{background:"rgba(255, 0, 0, 0)" , border:"solid rgba(255, 0, 0, 0)"}} >
   </div>
